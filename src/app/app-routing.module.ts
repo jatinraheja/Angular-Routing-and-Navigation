ff7844865fedbc4cd12353de4aacd0d69a9ec9a9
jatinraheja <jatinraheja1999@gmail.com>
2019-09-21 16:37:01 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DepartmentListComponent} from './department-list/department-list.component';
+import {EmployeListComponent} from './employe-list/employe-list.component';
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'departments' , component : DepartmentListComponent },
+  {path : 'employees' , component : EmployeListComponent },
+  {path : '**' , component : PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingcomponent = [DepartmentListComponent, EmployeListComponent, PagenotfoundComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule  , routingcomponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingcomponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
